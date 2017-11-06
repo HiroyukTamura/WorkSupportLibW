aefa9c10a29c35db2172acd751a0ce3b66c20e14
hirotamu3 <hirotamu3@gmail.com>
2017-11-06 18:12:50 +0900

    fabric, butterKnife導入

--- app/src/main/java/com/example/hiroyuki3/worksupportlibw/MainActivity.java
+++ app/src/main/java/com/example/hiroyuki3/worksupportlibw/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 }
